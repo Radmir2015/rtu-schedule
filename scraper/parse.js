45c794fb5888c519d6751cd6ab849df7622b7b08
Radmir2015 <ya.radmir2015@yandex.ru>
2019-10-04 09:02:54 +0400

    [add] options for parseXls; [fix] cell aiming during class parse

--- scraper/parse.js
+++ scraper/parse.js
@@ -5,71 +5,87 @@ const getValueOn = function (worksheet, columnN, rowN) {
     return worksheet[xlsx.utils.encode_cell({c: columnN, r: rowN})].v;
 }
 
-const parseXls = function (data) {
-    const wbook = xlsx.readFile('IK_3k_19_20_osen.xlsx');
-    // const wbook = xlsx.read(data, { type: 'binary' });
-    // console.log(wbook.SheetNames);
+const parseXls = function (options) {
+    const wbook = (() => {
+        if (options.way == 'file')
+            return xlsx.readFile(options.xlsxFilename);
+        else if (options.way == 'binary')
+            return xlsx.read(options.data, { type: 'binary' });
+        else
+            throw 'No type of data specified';
+    })();
+
     const worksheet = wbook.Sheets[wbook.SheetNames[0]];
 
     const rowsLength = xlsx.utils.decode_range(worksheet['!ref']).e.r;
-    const groupsNames = [];
-    const classDuration = [];
-    const schedule = {};
-
-    console.log(worksheet[xlsx.utils.encode_cell({c: 2, r: 3})].v)
-    console.log(worksheet[xlsx.utils.encode_cell({c: 0, r: 3})].v)
-    // console.log(xlsx.utils.decode_range(worksheet[xlsx.utils.encode_cell({c: 0, r: 6})]));
+    const schedule = {
+        timeOfClass: [],
+        groupsNames: [],
+    };
 
     const oneDaySpan = worksheet['!merges'].find(range => range.s.c === 0 && range.s.r == 3 );
     const oneDayCellsNum = oneDaySpan.e.r - oneDaySpan.s.r;
 
     for (let rowN = oneDaySpan.s.r; rowN < oneDaySpan.e.r; rowN += 2) {
-        classDuration.push([
+        schedule.timeOfClass.push([
             getValueOn(worksheet, oneDaySpan.s.c + 2, rowN),
             getValueOn(worksheet, oneDaySpan.s.c + 3, rowN),
         ]);
     }
 
-    schedule.timeOfClass = classDuration;
-
-    console.log(classDuration);
-
     for (let colN = 0; colN < rowsLength; colN += 17) {
         [5, 9, 13].forEach(shift => {
-            // let address = xlsx.utils.encode_cell({c: colN + shift, r: 1})
-            // groupName = worksheet[address].v;
             let groupName = getValueOn(worksheet, colN + shift, 1)
             if (groupName !== '')
-                groupsNames.push(groupName);
+                schedule.groupsNames.push(groupName);
+            else
+                return;
 
-            schedule[groupName] = [];
+            schedule[groupName] = { 'I': [], 'II': [] };
 
-            for (let dayN = 0; dayN < 7; dayN++) {
+            for (let dayN = 0; dayN < 6; dayN++) {
                 let scheduleClass = {};
                 let scheduleOfDay = { 'I': [], 'II': [] };
                 for (let classN = 0; classN < oneDayCellsNum; classN++) { // classDuration.length * 2
                     scheduleClass = {
-                        'name': getValueOn(worksheet, colN + shift, 3 + classN),
-                        'type': getValueOn(worksheet, colN + shift + 1, 3 + classN),
-                        'teacher': getValueOn(worksheet, colN + shift + 2, 3 + classN),
-                        'classRoom': getValueOn(worksheet, colN + shift + 3, 3 + classN),
+                        'name': getValueOn(worksheet, colN + shift, 3 + classN + dayN * oneDayCellsNum),
+                        'type': getValueOn(worksheet, colN + shift + 1, 3 + classN + dayN * oneDayCellsNum),
+                        'teacher': getValueOn(worksheet, colN + shift + 2, 3 + classN + dayN * oneDayCellsNum),
+                        'classRoom': getValueOn(worksheet, colN + shift + 3, 3 + classN + dayN * oneDayCellsNum),
                     }
                     scheduleOfDay[classN % 2 ? 'II' : 'I'].push(scheduleClass);
                 }
-                schedule[groupName].push(scheduleOfDay);
+                // schedule[groupName].push(scheduleOfDay);
+
+                // schedule[groupName]['I'] = [...schedule[groupName]['I'], scheduleOfDay['I']];
+                // schedule[groupName]['II'] = [...schedule[groupName]['II'], scheduleOfDay['II']];
+
+                // schedule[groupName] = {
+                //     'I': [...schedule[groupName]['I'], scheduleOfDay['I']],
+                //     'II': [...schedule[groupName]['II'], scheduleOfDay['II']]
+                // }
+
+                schedule[groupName]['I'].push(scheduleOfDay['I']);
+                schedule[groupName]['II'].push(scheduleOfDay['II']);
             }
         })
     }
 
-    schedule.groupsNames = groupsNames;
+    // console.log(groupsNames);
+    // console.log(schedule);
 
-    console.log(groupsNames);
-    console.log(schedule);
+    if (options.saveToFile)
+        fs.writeFile(options.jsonFilename || 'schedule.json', JSON.stringify(schedule), err => {
+            if (err) console.error(err);
+            console.log('File schedule.json was written successfully!');
+        });
 
-    fs.writeFile('schedule.json', JSON.stringify(schedule), err => {
-        if (err) console.error(err);
-        console.log('File schedule.json was written successfully!');
-    });
+    return schedule;
 }
 
-parseXls();
+parseXls({
+    'way': 'file',
+    'xlsxFilename': 'IK_3k_19_20_osen.xlsx',
+    'saveToFile': true,
+    'jsonFilename': 'schedule.json'
+});
