1f15cb5b5cde23ababa22be7a857f584e39416f8
Radmir2015 <ya.radmir2015@yandex.ru>
2019-11-25 23:21:13 +0300

    [add] ambiguous groupname handler; [edit] router's urls

--- server/index.js
+++ server/index.js
@@ -13,22 +13,32 @@ app.set('json spaces', 2);
 
 // console.log = function() {}
 
-app.get('/', (req, res) => {
+app.get('/api/info', (req, res) => {
     // res.end('Use "/ + group name" to get json of schedule of the group.');
     res.json(parseInfo);
 })
 
-app.get('/fullFlattenedSchedule', (req, res) => {
+app.get('/api/fullFlattenedSchedule', (req, res) => {
     res.json(flattenedSchedule);
 })
 
-app.get('/fullSchedule', (req, res) => {
+app.get('/api/fullSchedule', (req, res) => {
     res.json(schedules);
 })
 
-app.get('/:groupName', (request, response) => {
-    const group = request.params.groupName.toUpperCase();
+app.get('/api/:groupName?', (request, response) => {
     let flag = false;
+    let group = request.params.groupName || request.query.groupName;
+    
+    if (group === undefined) {
+        response.json({
+            success: false,
+            error: 'No groupname specified.',
+        });
+        return;
+    }
+
+    group = group.toUpperCase();
     console.log(group);
 
     // console.log(schedules);
@@ -38,11 +48,25 @@ app.get('/:groupName', (request, response) => {
 
         if (schedule === null) continue;
 
-        const foundName = schedule.groupsNames.find(elem => elem.indexOf(group) !== -1);
+        const foundName = schedule.groupsNames.filter(elem => elem.toUpperCase().indexOf(group) !== -1);
 
-        if (!foundName) continue;
+        if (foundName.length === 0) continue;
 
-        response.status(200).json(schedule[foundName]);
+        if (foundName.length > 1) {
+            response.status(200).json({
+                success: false,
+                severalGroups: true,
+                groups: foundName,
+                error: 'Ambiguous groupname.'
+            });
+            break;
+        }
+
+        response.status(200).json({
+            success: true,
+            timeOfClass: schedule.timeOfClass,
+            ...schedule[foundName],
+            });
 
         // console.log(schedule[foundName]);
         flag = true;
@@ -50,15 +74,20 @@ app.get('/:groupName', (request, response) => {
     }
 
     if (!flag) {
-        response.write(`Groupname ${group} hasn't been found.`);
-        response.end();
+        // response.write(`Groupname ${group} hasn't been found.`);
+        // response.end();
+        response.json({
+            success: false,
+            error: `Groupname ${group} hasn't been found.`,
+        })
     }
 })
 
 const startParsing = (info) => new Promise((resolve, reject) => resolve(require('../scraper/scrape')(info)));
 
-app.get('/startParse', (res, req) => {
-    let parseInfo = { parsed: 0, error: 0, total: 0, startedTime: new Date(), finishedTime: new Date(), finished: false, elapsed: 0 };
+app.get('/startParse', (req, res) => {
+    parseInfo = { parsed: 0, error: 0, total: 0, startedTime: new Date(), finishedTime: new Date(), finished: false, elapsed: 0 };
+    res.json(parseInfo);
     console.log('Start scraping...');
     startParsing(parseInfo).then((sched) => {
         schedules = sched;
@@ -76,24 +105,25 @@ app.get('/startParse', (res, req) => {
 app.listen(port, () => {
     console.log(`Server is running on ${port}...`);
 
-    console.log('Start scraping...');
+    // console.log('Start scraping...');
     // (async (info) => {
     //     schedules = await require('../scraper/scrape')(info);
     //     info.finished = true; info.finishedTime = new Date(); info.elapsed = (info.finishedTime.getTime() - info.startedTime.getTime()) / 1000;
     //     console.log(`Scraped ${schedules.length} tables.`);
     //     console.log('End of scraping...');
     // })(parseInfo);
-    startParsing(parseInfo).then((sched) => {
-        schedules = sched;
-        parseInfo.finished = true; parseInfo.finishedTime = new Date(); parseInfo.elapsed = (parseInfo.finishedTime.getTime() - parseInfo.startedTime.getTime()) / 1000;
-        console.log(`Scraped ${schedules.length} tables.`);
-        console.log('End of scraping...');
-        flattenedSchedule = [];
-        schedules.forEach(aSchedule => {
-            if (aSchedule !== undefined)
-                flattenedSchedule = [...flattenedSchedule, ...aSchedule];
-        });
-    })
+
+    // startParsing(parseInfo).then((sched) => {
+    //     schedules = sched;
+    //     parseInfo.finished = true; parseInfo.finishedTime = new Date(); parseInfo.elapsed = (parseInfo.finishedTime.getTime() - parseInfo.startedTime.getTime()) / 1000;
+    //     console.log(`Scraped ${schedules.length} tables.`);
+    //     console.log('End of scraping...');
+    //     flattenedSchedule = [];
+    //     schedules.forEach(aSchedule => {
+    //         if (aSchedule !== undefined)
+    //             flattenedSchedule = [...flattenedSchedule, ...aSchedule];
+    //     });
+    // })
 });
 
 // app.on('listening', () => {
